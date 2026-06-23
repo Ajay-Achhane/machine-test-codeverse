728966321a242721911be3f8fb59416c36c5f38b
Ajayachhane03 <161864755+Ajay-Achhane@users.noreply.github.com>
2026-06-23 13:09:37 +0530

    Delete Videos/Machine_test21 directory

--- Videos/Machine_test21/utility/generatetoken.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-const SECRET_KEY = "test-secret-key12345";
-
-const generateToken = (payload) => {
-  return jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
-};
-
-module.exports = generateToken;
